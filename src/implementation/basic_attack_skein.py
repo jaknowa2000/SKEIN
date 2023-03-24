83788cd97e0ab46a91eef5f78cd88b718a7ef505
Kuba <jaknowa2000@gmail.com>
2023-03-24 10:50:26 +0100

    Refactoring function show_stats().

--- src/implementation/basic_attack_skein.py
+++ src/implementation/basic_attack_skein.py
@@ -4,6 +4,10 @@ import multiprocessing
 from multiprocessing.sharedctypes import Array, Value
 from math import ceil, log2
 
+"""Set time of attack in seconds and nuber of attacked bytes below"""
+ATTACK_TIME = 30
+NB = 4
+
 
 def attack_skein(start_val, number_messages):
     start_message = bytes(start_val)
@@ -15,23 +19,28 @@ def attack_skein(start_val, number_messages):
     while True:
         modified_message = modified_message.to_bytes(length=ceil((log2(modified_message+1))/8), byteorder="big")
         current_hash = skein.skein_512_512("", modified_message)
-        if attacked_hash[:4] == current_hash[:4]:
+        if attacked_hash[:NB] == current_hash[:NB]:
             break
         modified_message = int.from_bytes(modified_message, "big")
         modified_message += 1
         number_messages.value += 1
     stop = time.time()
-    print("Time of attack: ", (stop-start)/(60*60), "[h]\n")
-    print("Attacked skein hash on first 3 bytes: ", end=" ")
-    for i in attacked_hash[:4]:
+    attacked_time = stop - start
+    show_stats(attacked_time, attacked_hash, current_hash, modified_message, number_messages)
+
+
+def show_stats(attacked_time, attacked_hash, current_hash, modified_message, number_messages):
+    print("Time of attack: ", attacked_time/(60*60), "[h]\n")
+    print(f"Attacked skein hash on first {NB} bytes: ", end=" ")
+    for i in attacked_hash[:NB]:
         print(hex(i), end=" ")
-    print("\n\nObtained hash on first 3 bytes: ", end=" ")
-    for i in current_hash[:4]:
+    print(f"\n\nObtained hash on first {NB} bytes: ", end=" ")
+    for i in current_hash[:NB]:
         print(hex(i), end=" ")
     print("\nMessage that gave this hash: ")
     for i in range(0, len(modified_message), 20):
         print(list(map(hex, modified_message[i:20 + i])))
-    print("\nNumber of hashed messages: ", number_messages.value)
+    print("\nNumber of hashed messages: ", int(number_messages.value))
 
 
 def main():
@@ -40,10 +49,9 @@ def main():
     number_messages = Value("d", 0)
     proces_attack = multiprocessing.Process(target=attack_skein, name="Attack_skein", args=(message, number_messages))
     proces_attack.start()
-    attack_time = 30
-    proces_attack.join(attack_time)
+    proces_attack.join(ATTACK_TIME)
     if proces_attack.is_alive():
-        print("\nAllowable attack time: ", attack_time/(60*60), "[h] has elapsed")
+        print("\nAllowable attack time: ", ATTACK_TIME/(60*60), "[h] has elapsed")
         print("Number of hashed messages: ", int(number_messages.value))
         proces_attack.terminate()
         proces_attack.join()
--- src/implementation/collision_finding.py
+++ src/implementation/collision_finding.py
@@ -5,9 +5,12 @@ from multiprocessing.sharedctypes import Value
 from math import ceil, log2
 from skein import Skein
 
+"""Set time of attack in seconds and nuber of attacked bytes below"""
+ATTACK_TIME = 30
+NB = 4
+
 
 def collision_skein(number_messages):
-    nb = 4
     modified_message = random.randint(2 ** 800, 2 ** 801)
     hash_list = []
     message_list = []
@@ -17,23 +20,28 @@ def collision_skein(number_messages):
         message_list.append(modified_message)
         skein = Skein()
         current_hash = skein.skein_512_512("", modified_message)
-        if current_hash[:nb] in hash_list:
-            index = hash_list.index(current_hash[:nb])
+        if current_hash[:NB] in hash_list:
+            index = hash_list.index(current_hash[:NB])
             break
-        hash_list.append(current_hash[:nb])
+        hash_list.append(current_hash[:NB])
         modified_message = int.from_bytes(modified_message, "big")
         modified_message += 1
         number_messages.value += 1
     stop = time.time()
-    print("Time of attack: ", (stop-start)/(60*60), "[h]\n")
+    attack_time = stop - start
+    show_stats(attack_time, modified_message, message_list, hash_list, current_hash, index, number_messages)
+
+
+def show_stats(attack_time, modified_message, message_list, hash_list, current_hash, index, number_messages):
+    print("Time of attack: ", attack_time/(60*60), "[h]\n")
     print("First message of collision: ")
     for index1, i in enumerate(modified_message):
         if (index1 + 1) % 32 == 0:
             print(hex(i), end="\n")
         else:
             print(hex(i), end=" ")
-    print(f"\n\nHash of this message on first {nb} bytes: ", end=" ")
-    for i in current_hash[:nb]:
+    print(f"\n\nHash of this message on first {NB} bytes: ", end=" ")
+    for i in current_hash[:NB]:
         print(hex(i), end=" ")
     print("\n\nSecond message of collision: ")
     for index1, i in enumerate(message_list[index]):
@@ -41,7 +49,7 @@ def collision_skein(number_messages):
             print(hex(i), end="\n")
         else:
             print(hex(i), end=" ")
-    print(f"\n\nHash of this message on first {nb} bytes: ", end=" ")
+    print(f"\n\nHash of this message on first {NB} bytes: ", end=" ")
     for i in hash_list[index]:
         print(hex(i), end=" ")
     print("\n\nNumber of hashed messages: ", int(number_messages.value))
@@ -51,10 +59,9 @@ def main():
     number_messages = Value("d", 0)
     proces_attack = multiprocessing.Process(target=collision_skein, name="Attack_skein", args=(number_messages,))
     proces_attack.start()
-    attack_time = 30
-    proces_attack.join(attack_time)
+    proces_attack.join(ATTACK_TIME)
     if proces_attack.is_alive():
-        print("\nAllowable attack time: ", attack_time/(60*60), "[h] has elapsed")
+        print("\nAllowable attack time: ", ATTACK_TIME/(60*60), "[h] has elapsed")
         print("Number of hashed messages: ", int(number_messages.value))
         proces_attack.terminate()
         proces_attack.join()
